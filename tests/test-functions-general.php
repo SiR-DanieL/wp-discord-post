49da8b84112f395646a60fc33c31fa4a2da2ae62
Nicola Mustone <nicola.mustone@automattic.com>
2018-08-22 12:50:29 +0300

    Added test for wp_discord_post_is_embed_enabled

--- tests/test-functions-general.php
+++ tests/test-functions-general.php
@@ -22,4 +22,17 @@ class Test_Functions_General extends WP_UnitTestCase {
 
 		$this->assertFalse( wp_discord_post_is_logging_enabled() );
 	}
+
+	/**
+	 * @covers wp_discord_post_is_embed_enabled
+	 */
+	public function test_wp_discord_post_is_embed_enabled() {
+		add_option( 'wp_discord_post_disable_embed', 'yes' );
+
+		$this->assertTrue( wp_discord_post_is_embed_enabled() );
+
+		update_option( 'wp_discord_post_disable_embed', '' );
+
+		$this->assertFalse( wp_discord_post_is_embed_enabled() );
+	}
 }
